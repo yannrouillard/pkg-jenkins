c37165221b772bb735caf5b799bbfb1320601036
James Page <james.page@canonical.com>
2012-01-05 12:13:52 +0000

    Added some missing exclusions to orig-tar.sh

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -13,7 +13,8 @@ GZIP=--best tar -czf $TAR --exclude '*.jar' --exclude '*.class' \
      --exclude 'CVS' --exclude '.svn' --exclude "jenkins-$VERSION/debian" \
      --exclude "jenkins-$VERSION/test" --exclude "jenkins-$VERSION/osx" \
      --exclude "jenkins-$VERSION/ebuild" --exclude "jenkins-$VERSION/opensuse" \
-     --exclude "jenkins-$VERSION/rpm" --exclude 'hudson-logo.vsd' \
+     --exclude "jenkins-$VERSION/rpm" --exclude "jenkins-$VERSION/ips" \
+     --exclude "jenkins-$VERSION/msi" --exclude 'hudson-logo.vsd' \
      --exclude 'TangoProject-License.url' --exclude 'dummy.keystore' $DIR
 
 rm -rf $DIR
