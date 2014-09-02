0777fba74124c80f1aaf943d9e4ae859835b4b68
Emmanuel Bourg <ebourg@apache.org>
2014-09-02 12:13:28 +0200

    Use XZ compression for the upstream tarball

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -1,21 +1,31 @@
 #/bin/sh -e
 
 VERSION=$2
-TAR=../jenkins_$VERSION.orig.tar.gz
+TAR=../jenkins_$VERSION.orig.tar.xz
 DIR=jenkins-$VERSION
 mkdir -p $DIR
 
 # Unpack ready fo re-packing
-tar -xzf $TAR -C $DIR --strip-components=1
+tar -xzf $3 -C $DIR --strip-components=1
+rm $3
 
 # Repack excluding stuff we don't need
-GZIP=--best tar -czf $TAR --exclude '*.jar' --exclude '*.class' \
-     --exclude 'CVS' --exclude '.svn' --exclude "jenkins-$VERSION/debian" \
-     --exclude "jenkins-$VERSION/test" --exclude "jenkins-$VERSION/osx" \
-     --exclude "jenkins-$VERSION/ebuild" --exclude "jenkins-$VERSION/opensuse" \
-     --exclude "jenkins-$VERSION/rpm" --exclude "jenkins-$VERSION/ips" \
-     --exclude "jenkins-$VERSION/msi" --exclude 'hudson-logo.vsd' \
-     --exclude 'TangoProject-License.url' --exclude 'dummy.keystore' $DIR
+XZ_OPT=--best tar -cJf $TAR \
+     --exclude '*.jar' \
+     --exclude '*.class' \
+     --exclude 'CVS' \
+     --exclude '.svn' \
+     --exclude "jenkins-$VERSION/debian" \
+     --exclude "jenkins-$VERSION/test" \
+     --exclude "jenkins-$VERSION/osx" \
+     --exclude "jenkins-$VERSION/ebuild" \
+     --exclude "jenkins-$VERSION/opensuse" \
+     --exclude "jenkins-$VERSION/rpm" \
+     --exclude "jenkins-$VERSION/ips" \
+     --exclude "jenkins-$VERSION/msi" \
+     --exclude 'hudson-logo.vsd' \
+     --exclude 'TangoProject-License.url' \
+     --exclude 'dummy.keystore' \
+     $DIR
 
 rm -rf $DIR
-
